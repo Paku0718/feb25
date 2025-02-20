ef5edff0bfbe1dd8bb3b0398b165dc7a94df4566
Paras Dhananjay Kuranjekar <paraskuranjekar@gmail.com>
2025-02-20 15:48:09 +0530

    commit

---
+++ 20feb/Laptop1.java
@@ -0,0 +1,44 @@
+class Laptop1 {
+	int ram;
+	String name;
+	double price;
+	
+	Laptop1(){
+		ram = 8;
+		name = "HP";
+		price = 50000.0;
+		System.out.println("in laptop constr.");
+	}
+	
+	Laptop1(int a, String s, double d){
+		ram = a;
+		name = s;
+		price = d;
+		System.out.println("in laptop 3 para constr.");
+	}
+
+	
+
+	void accessCamera(){
+		System.out.println(" in accessCamera()");
+	}
+	
+	String display(){
+		//System.out.println(" inside display");
+		return "Details of Laptop : " + ram + " " + name + " " + price;
+		
+	}
+
+}
+
+class LaptopDemo1{
+	public static void main(String args[]){
+		int a=10;
+		Laptop1 laptopObj = new Laptop1(); //heap
+
+		Laptop1 laptopObj1 = new Laptop1(16, "Dell", 6000.0);
+
+		System.out.println(laptopObj.display());
+		System.out.println(laptopObj1.display());
+	}
+}
